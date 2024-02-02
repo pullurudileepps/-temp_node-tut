c4b56a83932426d8f19a667424dd5e21226ff9f0
pullurudileepps <157283596+pullurudileepps@users.noreply.github.com>
2024-02-03 00:18:10 +0530

    promise code changes

--- fs-promise-async.js
+++ fs-promise-async.js
@@ -1,4 +1,27 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises;
+//const util = require('util');
+
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromis = util.promisify(writeFile);
+
+/* approach 1 */
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt','utf8');
+        const second = await readFile('./content/second.txt','utf8');
+        await writeFile('./content/dileep.txt', 
+        `This is awesome : ${first} ${second}`)
+        console.log(first,second)
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+start()
+/* approach 2 */
+
+/*
 const getText = (path) => {
     return new Promise((resolve,reject) => {
         readFile(path,'utf8',(err,data) => {
@@ -12,14 +35,19 @@ const getText = (path) => {
 
 const start = async () => {
     try {
-        const first = await getText('./content/result-async.txt');
-        console.log(first)
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+        
+        console.log(first,second)
     }
     catch (err) {
         console.log(err);
     }
 }
 start()
+
 // getText('./content/result-async.txt').
 // then(res => console.log(res)).
 // catch(err => console.log(err))
+
+*/
